9a5d23dc3e9e6c4667b574e14ad0e2cd4fa1882c
mRanas123 <134073126+mRanas123@users.noreply.github.com>
2025-05-30 15:06:13 +0300

    Update settings.py

--- Fix_it/Fix_it_app/settings.py
+++ Fix_it/Fix_it_app/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
